351264a1d83ad7308834356ed2cb7dede8793443
syvolapenko <isivolap@gmail.com>
2023-04-01 22:53:47 +0300

    first commit

---
+++ src/main/java/com/group10/PersonId.java
@@ -0,0 +1,11 @@
+package com.group10;
+
+public class PersonId {
+    private String name;
+    private String lastName;
+
+    public PersonId(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
